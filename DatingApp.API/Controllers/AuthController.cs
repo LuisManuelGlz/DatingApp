d3238a591ea09a64a5e1cc30c140f17a17e09b3a
Luis Manuel <mannyglzg@gmail.com>
2019-11-27 20:09:03 -0800

    Register arreglado
    Tomaba el nombre de usuario en lugar de la contraseña y generaba un
    hash incorrecto

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
+    // http://localhost:5000/api/auth/
     [Route("api/[controller]")]
     [ApiController]
 
@@ -18,22 +19,33 @@ namespace DatingApp.API.Controllers
             _repo = repo;
         }
 
+        // http://localhost:5000/api/auth/register/
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
+            /*
+                userFprRegisterDto se definen sus valores al hacer post
+                userForRegisterDto tiene consigo username y password
+            */
+
             // validate request
 
+            // pasamos el nombre de usuario a lower
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
+            // verificamos si existe
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("User already exists");
 
+            // seteamos un nuevo usuario solamente con su username
             var userToCreate = new User
             {
                 Username = userForRegisterDto.Username
             };
 
-            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Username);
+            // registramos un nuevo usuario
+            // Register recibe el usuario para crear (userToCreate) y la contraseña introducida
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
         }
--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
@@ -16,11 +15,13 @@ namespace DatingApp.API.Data
 
         public async Task<User> Login(string username, string password)
         {
+            // obtenemos el usuario o null
             var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
 
             if (user == null)
                 return null;
 
+            // verificamos si la contraseña coincide
             if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
                 return null;
 
@@ -29,12 +30,16 @@ namespace DatingApp.API.Data
 
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
+            // obtenemos el HMAC con la salt
             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
+                // como ya introducimos la salt obtenemos el hash de la contraseña
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
 
                 for (int i = 0; i < computedHash.Length; i++)
                 {
+                    // verificamos cada byte
+                    // si uno no coincide mandamos un false
                     if (computedHash[i] == passwordHash[i]) return false;
                 }
             }
@@ -46,26 +51,33 @@ namespace DatingApp.API.Data
             byte[] passwordHash, passwordSalt;
             CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
+            // asignamos los bytes a user
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
 
-            await _context.AddAsync(user);
-            await _context.SaveChangesAsync();
+            await _context.AddAsync(user); // agregamos
+            await _context.SaveChangesAsync(); // y guardamos
 
             return user;
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
+            // creamos HMAC
             using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
+                // obtiene la llave en bytes
                 passwordSalt = hmac.Key;
+                
+                // convierte la contraseña en bytes y la hashea
+                // passwordHash termina siendo bytes
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
 
         public async Task<bool> UserExists(string username)
         {
+            // verificamos si existe
             if (await _context.Users.AnyAsync(x => x.Username == username))
                 return true;
 
