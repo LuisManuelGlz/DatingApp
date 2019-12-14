26239ba94a8e4994391f82e4fffbd51b99b6f119
Luis Manuel <mannyglzg@gmail.com>
2019-12-13 19:00:24 -0800

    Se agregaron nuevas librerías y se modificaron los estilos

--- DatingApp-SPA/src/app/components/register/register.component.ts
+++ DatingApp-SPA/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,10 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) { }
 
   ngOnInit() {
   }
@@ -19,14 +23,13 @@ export class RegisterComponent implements OnInit {
   register() {
     this.authService.register(this.model)
       .subscribe(() => {
-        console.log('Registration successful');
+        this.alertify.success('Registration successful');
       }, error => {
-        console.log(error);
+        this.alertify.error(error);
       });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('Cancelled');
   }
 }
---
+++ DatingApp-SPA/src/app/services/alertify.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import * as alertify from "alertifyjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+
+  constructor() { }
+
+  confirm(message: string, okCallback: () => any): void {
+    alertify.confirm(message, (e, any) => {
+      if (e) {
+        okCallback();
+      } else {
+        
+      }
+    });
+  }
+
+  success(message: string): void {
+    alertify.success(message);
+  }
+
+  error(message: string): void {
+    alertify.error(message);
+  }
+
+  warning(message: string): void {
+    alertify.warning(message);
+  }
+
+  message(message: string): void {
+    alertify.message(message);
+  }
+
+  
+}
